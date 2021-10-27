fcf90037a4427ac00ddad9b481d850d64486bbcc
itsTurnip <22935812+itsTurnip@users.noreply.github.com>
2021-10-27 10:47:28 +0500

    Divide some search queries

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ if (!String.prototype.format) {
 }
 
 function validateDomain(uri) {
-    return /^(((?!-))(xn--|_{1,1})?[a-z0-9-]{0,61}[a-z0-9]{1,1}\.)*(xn--)?([a-z0-9][a-z0-9\-]{0,60}|[a-z0-9-]{1,30}\.[a-z]{2,})$/.test(uri);
+    return /^(((?!-))(xn--|_{1,1})?[a-z0-9-]{0,61}[a-z0-9]{1,1}\.)+(xn--)?([a-z0-9][a-z0-9\-]{0,60}|[a-z0-9-]{1,30}\.[a-z]{2,})$/.test(uri);
 }
 
 
